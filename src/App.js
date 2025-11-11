936056347a90d9c5400b947029b392c301a5deef
Anupriya2k1 <anupriya512001@gmail.com>
2025-11-11 12:14:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello World</h1>
+      <p>React is running live 🚀</p>
+    </div>
+  );
+}
+
+export default App;
